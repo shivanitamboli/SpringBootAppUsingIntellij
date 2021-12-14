5fc0a6015bebfeb02e07097507336adbe8ec6dc5
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-12-14 21:16:03 +0530

    Create First Spring Boot Apllication

---
+++ src/test/java/com/example/firstspringbootapp/FirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
